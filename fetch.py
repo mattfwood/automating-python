9fc6b9095745a502d8a390e7815d36387210708f
Matt Wood <matt@cratebind.com>
2020-07-24 10:07:16 -0500

    Update fetch example, update readme

--- fetch.py
+++ fetch.py
@@ -1,38 +1,50 @@
 import requests
 import urllib.parse
+import webbrowser
+
 import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-QUESTION_ENDPOINT = (
-    'https://api.stackexchange.com/2.2/search?order=desc&sort=votes&site=stackoverflow'
-)
+# @TODO: get this from user input
+USER = 'mattfwood'
+ENDPOINT = f"https://api.github.com/users/{USER}/repos?sort=pushed"
 
-# @TODO: Get this from user
-query = 'python filter'
+response = requests.get(ENDPOINT)
+result = response.json()[0]
 
+print(result['url'])
+webbrowser.open(result['svn_url'])
 
-def pretty_print(value):
-    return pp.pprint(value)
+# QUESTION_ENDPOINT = (
+#     'https://api.stackexchange.com/2.2/search?order=desc&sort=votes&site=stackoverflow'
+# )
 
-
-def get_question(query):
-    question_response = requests.get(QUESTION_ENDPOINT, params={'intitle': query})
-    first_result = question_response.json()['items'][0]
-    return first_result
-
-
-def get_top_answer(question_id):
-    response = requests.get(
-        f"https://api.stackexchange.com/2.2/questions/{question_id}/answers?order=desc&sort=votes&site=stackoverflow&filter=!9_bDE(S6I"
-    )
-    result = response.json()['items'][0]['body_markdown']
-    return result
+# # @TODO: Get this from user
+# query = 'python filter'
 
 
-result = get_question(query)
-top_answer = get_top_answer(result['question_id'])
+# def pretty_print(value):
+#     return pp.pprint(value)
 
-# pretty_print(result)
-pretty_print(top_answer)
+
+# def get_question(query):
+#     question_response = requests.get(QUESTION_ENDPOINT, params={'intitle': query})
+#     first_result = question_response.json()['items'][0]
+#     return first_result
+
+
+# def get_top_answer(question_id):
+#     response = requests.get(
+#         f"https://api.stackexchange.com/2.2/questions/{question_id}/answers?order=desc&sort=votes&site=stackoverflow&filter=!9_bDE(S6I"
+#     )
+#     result = response.json()['items'][0]['body_markdown']
+#     return result
+
+
+# result = get_question(query)
+# top_answer = get_top_answer(result['question_id'])
+
+# # pretty_print(result)
+# pretty_print(top_answer)
 # print(result)
--- input.py
+++
@@ -1,2 +0,0 @@
-# API_ENDPOINT = f"https://api.stackexchange.com/2.2/search?order=desc&sort=activity&site=stackoverflow&intitle=python%20input"
-
